df36fc133c8b6b8ca160761db77c74b6af6f1e58
Utkarsh Shakya <89725446+utkarshakya@users.noreply.github.com>
2023-11-04 01:55:25 +0000

    basic primitive data types

---
+++ javaScript/02_dataTypes.js
@@ -0,0 +1,21 @@
+// Previously js don't have classes, modules and other new features but now it has so many new features and to deal with now for treating out code as an new standared code we use "use strict"
+"use strict" // treat all JS code as newer version of code.
+
+// alert("Hello") // this one is a browser function we can't use it with node.js
+
+// Now there are not many data types in JS
+
+// Primitive Data Types :
+let age = 23 // Number Data Type
+let name = "Samentha" // String Data Type
+let isLogedIn = true // Boolean Data Type
+let state; // Undefined
+let value = null // Standalone Value
+// There is one more data type namely bigint for large numbers
+// And Symbols --> this one is unique we will learn about it soon
+
+// We can use typeof() function to see the data type of any variable
+console.table(typeof(age));
+
+// Non-Primitive Data Types OR Objects :
+// Objects are very important in JS we will learn about them soon
