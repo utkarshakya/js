4a518e96bb569b75a95cf151922f89e0f3629ddc
Utkarsh Shakya <89725446+utkarshakya@users.noreply.github.com>
2023-11-08 02:25:38 +0000

    Conversion and Operation basics done

---
+++ javaScript/03_conversionOperation.js
@@ -0,0 +1,51 @@
+// Uncomment the specific multiline comment to see the output. Bydefault last one will runs.
+
+// ****** CONVERSIONS *******
+
+/*
+let c = "34"; // This is string
+let n = Number(c); // Explicit conversion
+console.log("Type of n if c = '34' and n = Number(c) : " + typeof n); // This will simply give number
+
+c = "34Mk";
+n = Number(c);
+console.log("Type of n if c = '34Mk' and n = Number(c) : " + typeof n); // Note the type and the value of this one
+console.log("Value of n if c = '34Mk' and n = Number(c) : " + n); // NaN -> Not a Number
+
+c = "";
+n = Boolean(c);
+console.log("Value of n if c = '' and n = Boolean(c) : " + n); // In js empty string are also False
+
+c = null;
+n = Boolean(c);
+console.log("Type of n if c = null and n = Boolean(c) : " + typeof n);
+console.log("Value of n if c = null and n = Boolean(c): " + n);
+*/
+// We have explicit type conversion funcion of all data types except null and undefined.
+
+// ****** OPERATIONS *******
+
+/*
+// Basic Mathematical Operations
+console.log(2 + 3); // Addition
+console.log(2 - 3); // Subtraction
+console.log(2 * 3); // Multiplication
+console.log(2 / 3); // Division
+console.log(2 % 3); // Modulo
+console.log(2 ** 3); // Exponent
+*/
+
+/*
+// Basic String Operations
+console.log("Hello" + " Moto"); // concatination
+// console.log("Ram"*2); // Replication not allowed
+*/
+
+
+// Some Other Concepts
+console.log(2 + "4"); // Implicit conversion
+console.log("2" + 4);
+console.log("2" + 4 + 2);
+console.log(2 + 4 + "2"); // Note the result, this will do arithmatic and the concatinate
+console.log(+true); // this is also a implicit conversion in js
+console.log(+""); // this is also a implicit conversion in js
