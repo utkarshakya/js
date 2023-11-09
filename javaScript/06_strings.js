4def5f0e83e7a88c4cb944951bca3a6aa0128bb7
Utkarsh Shakya <89725446+utkarshakya@users.noreply.github.com>
2023-11-09 02:05:48 +0000

    Strings

---
+++ javaScript/06_strings.js
@@ -0,0 +1,14 @@
+let name = "Samentha";
+let age = 35;
+
+// This is how I am writting a variable while printing its value with string, simply using
+// concatination but this is old way and can some time not very clean and readable.
+console.log("Hello My Name Is " + name + " And I Am " + age + " Years Old.");
+
+// So now days we use `` (Backticks) to write such things as give below
+console.log(`Hello My Name Is ${name} And I Am ${age} Years Old.`);
+// This is called STRING INTERPOLATION.
+// Although output is same for both but this is good practice.
+
+// ********** ****************************** **********
+// THERE ARE MANY METHOD OF STRING THAT WE CAN USE, Check mdn.
