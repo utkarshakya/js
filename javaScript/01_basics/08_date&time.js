602918b5f400e9f13b3e64ea7d62d549f5d06f2a
Utkarsh Shakya <89725446+utkarshakya@users.noreply.github.com>
2024-08-21 03:30:17 +0000

    01/08_data&time.js file is created and written by some code

---
+++ javaScript/01_basics/08_date&time.js
@@ -0,0 +1,22 @@
+// NOTE: Javascript TC39 is working on Temporal, a new Date/Time API.
+// TC39 working to make it a global object but for now Date object is used, in future it change.
+// Check this to know more --> http://rb.gy/lste6u
+
+
+const myDate = new Date()
+// This is how simply define a data, this will give my current data and time.
+console.log(myDate)
+
+// --- --- Using Some Methods Of Date --- ---
+console.log(myDate.getDay())
+console.log(myDate.getFullYear())
+console.log(myDate.getMonth())
+console.log(myDate.getDate())
+console.log(myDate.getHours())
+console.log(myDate.getMinutes())
+console.log(myDate.getSeconds())
+console.log(myDate.getMilliseconds())
+
+console.log(myDate.getTime())
+
+// To be continue...
