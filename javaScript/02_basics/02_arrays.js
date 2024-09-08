68fdd26ee90d5a35d4d3090259c260c7a184a6c3
Utkarsh Shakya <subhashshakya136@gmail.com>
2024-09-08 21:08:11 +0530

    Added javaScript\02_basics\02_arrays.js file.

---
+++ javaScript/02_basics/02_arrays.js
@@ -0,0 +1,60 @@
+// Uncomment The Specific Multi-Line Comment To See The Output, By Default Last One Runs.
+
+// --- --- --- MORE ON ARRAY --- --- ---
+
+/*
+let marvelHeros = ['Vision', 'Captain Marvel', 'Spider-Man'];
+let dcHeros = ['Victor', 'Wounder Women', 'Super-Man'];
+
+// --- --- concat() Method --- ---
+let allHeros = marvelHeros.concat(dcHeros);
+console.log(allHeros);
+// concat() method simply returns an new array after performing concatenation.
+
+// --- --- Spread Operator (...) --- ---
+let newHerosList = [...marvelHeros, ...dcHeros];
+console.log(newHerosList);
+// Spread Operator (...), In simple words spread the array or string into its individual elements.
+// See the output to understand clearly.
+*/
+
+/*
+let numbersArray = [1,2,3,[4,5,[6,7], 8], 9, 0];
+
+// --- --- flat() Method --- ---
+console.log(numbersArray.flat(Infinity));
+console.log(numbersArray.flat(1));
+console.log(numbersArray.flat()); // By default flat() takes 1 as argument.
+// flat() returns array in a simplified manner by removing the sub-arrays.
+// It takes one argument namely 'depth', which tell how many inner sub-arrays we simplify.
+// Here, Simplify --> means getting values of array after removing the square brackets [].
+// See the output of the above example to understand it better.
+*/
+
+// NOTE: ALL METHOD AND PROPERTIES DISCUSSED BEFORE THIS SECTION ARE KNOWN AS INSTANCE METHODS & PROPERTIES.
+// --- --- --- STATIC METHODS IN ARRAY --- --- ---
+
+// 1. Array.from() --> Creates a new Array instance from an iterable or array-like object.
+// 2. Array.isArray() --> Returns true if the argument is an array, or false otherwise.
+// 3. Array.of() --> Creates a new Array instance with a variable number of arguments, regardless of number or type of the arguments.
+
+console.log(Array.isArray("Amigo"));
+console.log(Array.isArray([3,4,5]));
+
+console.log(Array.from("Amigo"));
+
+let num1 = 45;
+let num2 = 78;
+console.log(Array.of("Amigo", 34, 381, "Galactus", num1, num2));
+// See the output to get them all.
+
+
+
+// REMEMBER:
+// Instance Methods & Properties: Called on instances of the object.
+// Static Methods & Properties: Called on the constructor itself.
+
+// check the stackoverflow answer given below to learn more about instance and static methods & properties
+// https://stackoverflow.com/questions/50251894/javascript-static-method-vs-instance-method
+
+// NOW WE HAVE DONE WITH ARRAYS.
