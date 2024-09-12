862a43063035716965e0889a7cbb7d1f413c87d0
Utkarsh Shakya <subhashshakya136@gmail.com>
2024-09-12 09:20:00 +0530

    Added javaScript\03_basics\02_scopes.js file.

---
+++ javaScript/03_basics/02_scopes.js
@@ -0,0 +1,10 @@
+{
+    let a = 34;
+    var b = 45;
+}
+// console.log(a) // This will not work since a is initialized in local scope.
+console.log(b) // This will work since we use var which make variable declaration global.
+
+// SEE THIS SMALL MDN DOCS. TO CLEAR SCOPE: https://developer.mozilla.org/en-US/docs/Glossary/Scope
+
+// --- SEE YOU IN THE NEXT ONE, BYE ---
