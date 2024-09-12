f85c8528af4fde3b14a5923d663962a105eb1765
Utkarsh Shakya <subhashshakya136@gmail.com>
2024-09-12 09:25:13 +0530

    Modified javaScript\03_basics\02_scopes.js file

--- javaScript/03_basics/02_scopes.js
+++ javaScript/03_basics/02_scopes.js
@@ -4,7 +4,7 @@
 }
 // console.log(a) // This will not work since a is initialized in local scope.
 console.log(b) // This will work since we use var which make variable declaration global.
+// This scope thing works everywhere from functions to objects to simple conditional statements
 
 // SEE THIS SMALL MDN DOCS. TO CLEAR SCOPE: https://developer.mozilla.org/en-US/docs/Glossary/Scope
-
 // --- SEE YOU IN THE NEXT ONE, BYE ---
