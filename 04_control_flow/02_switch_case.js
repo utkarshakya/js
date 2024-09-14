2eff3e788e8cb7328c7c09c5e439a516cfac265c
Utkarsh Shakya <subhashshakya136@gmail.com>
2024-09-14 08:10:21 +0530

    ADDED 04_control_flow\02_switch_case.js FILE.

---
+++ 04_control_flow/02_switch_case.js
@@ -0,0 +1,27 @@
+const day = 1
+switch(day){
+    case 1:
+        console.log('Monday')
+        break;
+    case 2:
+        console.log('Tuesday')
+        break;
+    case 3:
+        console.log('Wednesday')
+        break;
+    case 4:
+        console.log('Thursday')
+        break;
+    case 5:
+        console.log('Friday')
+        break;
+    case 6:
+        console.log('Saturday')
+        break;
+    case 7:
+        console.log('Sunday')
+        break;
+    default:
+        console.log('Not A Week Day')
+}
+// This is how simply switch case works. Change the value of day and see the output.
