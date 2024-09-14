30a286cecb3e16487895acc36b500072940d0f2a
Utkarsh Shakya <subhashshakya136@gmail.com>
2024-09-14 07:49:57 +0530

    ADDED 04_control_flow\01_if_else.js FILE.

---
+++ 04_control_flow/01_if_else.js
@@ -0,0 +1,31 @@
+if (true) {
+    console.log(`If Running`)
+} else {
+    console.log(`Else Running`)
+}
+
+if (false) {
+    console.log(`If Running`)
+} else {
+    console.log(`Else Running`)
+}
+// This is simply if-else statement, It is same as in other language.
+// We can also use else-if and nesting in the same way as we in C/C++ language.
+// Here is Real use case example of if else.
+const debitCard = true;
+const loggedInStatus = true;
+if(debitCard && loggedInStatus){
+    console.log(`You Are Eligible For Buying Our Course`);
+}
+else if(!loggedInStatus){
+    console.log(`You Need To Login First`)
+}
+else if(!debitCard){
+    console.log(`You Need To Enter Debit Card Details To Proceed`)
+}
+else{
+    console.log(`To Get Start Log In And Fill The Details`)
+}
+
+// Switch Case also work in the same way as we do in other high level language.
+// --- WE WILL SEE THAT IN THE NEXT ONE, BYE BYE ---
