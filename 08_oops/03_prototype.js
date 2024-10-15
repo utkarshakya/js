7f1555e5bde1fb016f3c56090f09a7010c83d93e
Utkarsh Shakya <subhashshakya136@gmail.com>
2024-10-15 13:27:19 +0530

    ADDED 08_oops\03_prototype.js FILE

---
+++ 08_oops/03_prototype.js
@@ -0,0 +1,9 @@
+// Every thing in javascript is object and object refers to null.
+function hello(){
+    console.log("Hello")
+}
+hello()
+hello.name = "kingpin"
+console.log(hello.name)
+
+// TO BE CONTINUE...
